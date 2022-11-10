69317b71df2d6af42cc1268a7df30a717e49b14f
Edgard <ed1013@bu.edu>
2022-11-10 15:23:03 -0500

    fixed error with double digit numbers

--- insertSong.sh
+++ insertSong.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 #Arguments <song name.mp3> <place in list>
+SAVEIFS=$IFS #to prevent spaces breaking the foor loop
+IFS=$(echo -en "\n\b")
 lower=0
-filenames=`ls ./*.mp3`
 if [ -f "$1" ]; then
-	reName="$2_$1"
+	filenames=`ls ./*.mp3`
+	reName="$(printf %02d $2) - $1"
 	mv $1 $reName
 	for file in $filenames
 	do
+		echo $file
 		index=$(echo $file | sed 's@^[^0-9]*\([0-9]\+\).*@\1@')
+		echo $index
 		re='^[0-9]+$'
 		if [[ $index =~ $re ]] ; then
 			#check if much lower than first item, if so, no need to increment
-			if [[ $(($2+1)) -lt $index ]] && [[ $lower -eq 0 ]]; then
+			if [[ $((${2#0}+1)) -lt ${index#0} ]] && [[ $lower -eq 0 ]]; then
 				break
 			else
 				lower=1
 			fi
-			if [[ $index -ge $2 ]]; then
+			if [[ ${index#0} -ge ${2#0} ]]; then
 				newName=$(echo $file|cut -d'-' -f 2)
-				newName="$((index+=1))_$newName"
+				newName="$(printf %02d $((${index#0}+1))) - $newName"
 				mv $file $newName
 			fi
 		fi
@@ -27,3 +31,4 @@ if [ -f "$1" ]; then
 else
 	echo "$1 Does not exist"
 fi
+IFS=$SAVEIFS
